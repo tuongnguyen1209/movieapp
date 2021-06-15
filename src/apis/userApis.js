2ee906400f644fe4e265e14def8ac6a7a253711d
tuongnguyen1209 <65478301+tuongnguyen1209@users.noreply.github.com>
2021-06-15 10:40:22 +0700

    edit apis to heruku

--- src/apis/userApis.js
+++ src/apis/userApis.js
@@ -1,17 +1,17 @@
 import axiosClient from "./axiosClient";
 
-const url = "http://localhost:7300/apis/v2/user/";
-// const urlHeruku = "https://my-api-movie.herokuapp.com/apis/v2/user/";
+// const url = "http://localhost:7300/apis/v2/user/";
+const urlHeruku = "https://my-api-movie.herokuapp.com/apis/v2/user/";
 
 const UserApis = {
   userLogin: (formuser) => {
-    return axiosClient.post(`${url}login`, formuser);
+    return axiosClient.post(`${urlHeruku}login`, formuser);
   },
   getUser: () => {
-    return axiosClient.get(`${url}getuser`);
+    return axiosClient.get(`${urlHeruku}getuser`);
   },
   userSignin: (userForm) => {
-    return axiosClient.post(`${url}register`, userForm);
+    return axiosClient.post(`${urlHeruku}register`, userForm);
   },
 };
 
